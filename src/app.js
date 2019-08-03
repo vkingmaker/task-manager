81bdb03d7392241fe9bb2501ea3c88a0b07d9731
Monday Victor A <vkingmaker1@gmail.com>
2019-08-03 22:22:17 +0100

    chore: add database connection
    This commit adds mongodb to the projects and establishes
    a connection between the server and the database
    
    Delivers [#chore]

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
-import fs from 'fs';
+import { MongoClient } from 'mongodb';
 
-console.log(fs);
+const uri = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+client.connect((error) => {
+  if (error) {
+    return console.log('could not connect to the database.');
+  }
+
+  const db = client.db(databaseName);
+
+  db.collection('users').insertOne({
+    name: 'Monday Victor',
+    age: 27,
+  });
+});
