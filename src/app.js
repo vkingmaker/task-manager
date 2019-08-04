b3e2829414ab673a0639cc8856481cf8660ee581
Monday Victor A <vkingmaker1@gmail.com>
2019-08-05 00:51:06 +0100

    chore: isolate router
    this commit separates concern to make the application more scalable
    utilizes async await to handle promises more elegantly
    
    Delivers [#chore]

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-console */
 import express from 'express';
 import logger from 'morgan';
-import User from './models/user';
-import Task from './models/task';
+import userRouter from './routers/user';
+import taskRouter from './routers/task';
 import './db/mongoose';
-
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,66 +11,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(logger('dev'));
 
-app.post('/users', (req, res) => {
-  const user = new User(req.body);
-
-  user.save().then(() => {
-    res.status(201).send(user);
-  }).catch((e) => {
-    res.status(400).send(e);
-  });
-});
-
-app.get('/users', (req, res) => {
-  User.find({}).then((response) => {
-    res.status(200).send(response);
-  }).catch((e) => {
-    res.status(500).send(e);
-  });
-});
-
-app.get('/users/:id', (req, res) => {
-  const { id } = req.params;
-
-  User.findById(id).then((user) => {
-    if (!user) return res.status(404).send();
-    res.send(user);
-  }).catch((e) => {
-    res.status(500).send(e);
-  });
-});
-
-app.post('/tasks', (req, res) => {
-  const task = new Task(req.body);
-
-  task.save().then(() => {
-    res.status(201).send(task);
-  }).catch((e) => {
-    res.status(400).send(e);
-  });
-});
-
-app.get('/tasks', (req, res) => {
-  Task.find({}).then((tasks) => {
-    if (!tasks) return res.status(404).send();
-
-    res.send(tasks);
-  }).catch((e) => {
-    res.status(500).send();
-  });
-});
-
-app.get('/tasks/:id', (req, res) => {
-  const { id } = req.params;
-
-  Task.findById(id).then((task) => {
-    if (!task) return res.status(404).send();
-
-    res.send(task);
-  }).catcg((e) => {
-    res.status(500).send(e);
-  });
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(PORT, () => {
   console.log(`server is up and running at ${PORT}`);
