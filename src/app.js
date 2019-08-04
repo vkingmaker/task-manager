5f6ba519306c68d14c897c63f6b4886b2f58d541
Monday Victor A <vkingmaker1@gmail.com>
2019-08-04 20:34:42 +0100

    feature: create tasks
    this commit enables users to create tasks
    
    Delivers [#tasks]

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import logger from 'morgan';
-import './db/mongoose';
 import User from './models/user';
+import Task from './models/task';
+import './db/mongoose';
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,12 +15,23 @@ app.post('/users', (req, res) => {
   const user = new User(req.body);
 
   user.save().then(() => {
-    res.send(user);
+    res.status(201).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+
+  task.save().then(() => {
+    res.status(201).send(task);
   }).catch((e) => {
     res.status(400).send(e);
   });
 });
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`server is up and running at ${PORT}`);
 });
